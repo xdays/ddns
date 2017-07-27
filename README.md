6302691c6296433ba75a91c3f462047b31efec07
xdays <easedays@gmail.com>
2017-07-27 13:25:34 +0800

    rework ddns.py and add cloudflare support

--- README.md
+++ README.md
@@ -1,19 +1,27 @@
 #简介
-基于[DNSPod API](https://www.dnspod.cn/docs/records.html#dns)的动态DNS更新脚本
+
+动态DNS更新脚本，目前支持DnsPod和CloudFlare
+
+* [DNSPod API](https://www.dnspod.cn/docs/records.html#dns)
+* [CloudFlare API](https://api.cloudflare.com/)
 
 #功能模块
+
 * 获取本机的公网地址
 * 更新DNS记录
 * crontab运行，如公网地址没变则不发起更新请求
 
 #配置介绍
-* `login_email`: 登陆邮箱
-* `login_password`: 登陆密码
-* `format`: 响应数据格式，如json
-* `domain_id`: 域名ID，可通过`curl -k https://dnsapi.cn/Domain.List -d "login_email=xxx&login_password=xxx"`获取
-* `record_id`: 记录ID，可通过`curl -k https://dnsapi.cn/Record.List -d "login_email=xxx&login_password=xxx&domain_id=xxx"`获取
-* `sub_domain`: 子域名记录明，如www
-* `record_line`: 线路名称，如默认
 
-#感谢
-感谢此[gist](https://gist.github.com/chuangbo/833369)作者
+通过环境变量配置脚本:
+
+* `PROVIDER`: DNS服务商，目前支持`dnspod`和`cloudflare`
+* `TOKEN_ID`: ID，对于DNSPod来说是token_id，对于CloudFlare说是用户邮箱
+* `TOKEN_KEY`: 验证密钥，对于DNSPod来说是token, 对于CloudFlare来说是api_key
+* `DOMAIN`: 域名，比如xdays.me
+* `RECORD`: 记录，比如www.xdays.me
+* `IP_TYPE`: 使用公网地址还是私网地址, 对应为`public`和`private`
+
+# 示例
+
+    PROVIDER=cloudflare TOKEN_ID='whoami' TOKEN_KEY='changeme' DOMAIN=xdays.me RECORD=p.xdays.me IP_TYPE=public ./ddns.py
--- ddns.py
+++ ddns.py
@@ -1,44 +1,56 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
  
-import httplib, urllib
+from urllib import urlencode
+import requests
 import socket
 import time
 import fcntl
 import struct
+import os
  
-type = 'public' #type of ip address, public or private
-params = dict(
-    login_email="email", # replace with your email
-    login_password="pass", # replace with your password
-    format="json",
-    domain_id=100, # replace with your domain_od, can get it by API Domain.List
-    record_id=100, # replace with your record_id, can get it by API Record.List
-    sub_domain="pi", # replace with your sub_domain
-    record_line="默认",
-)
-current_file = open('./current', 'r')
-current_ip = current_file.read()
-current_file.close()
- 
-def ddns(ip):
-    params.update(dict(value=ip))
-    headers = {"Content-type": "application/x-www-form-urlencoded", "Accept":
-"text/json"}
-    conn = httplib.HTTPSConnection("dnsapi.cn")
-    conn.request("POST", "/Record.Ddns", urllib.urlencode(params), headers)
-    
-    response = conn.getresponse()
-    print response.status, response.reason
-    data = response.read()
-    print data
-    conn.close()
-    return response.status == 200
+def dp_dns(token_id, token_key, domain, record, ip):
+    api_url = 'https://dnsapi.cn/'
+    global_params = { 'login_token': '%d,%s' % (token_id, token_key),
+        'format': 'json'
+    }
+    headers = {"Content-type": "application/x-www-form-urlencoded",
+        "Accept": "text/json"}
+    global_params.update({'domain': domain})
+    records = requests.post(api_url + 'Record.List', data=urlencode(global_params),
+        headers=headers).json()
+    record_id = [ i['id'] for i in records['records'] if i['name'] == record ][0]
+    global_params.update({ 'record_id': record_id,
+        'sub_domain': record,
+        'record_line_id': 0,
+        'value': ip
+    })
+    result = requests.post(api_url + 'Record.Ddns',
+        data=urlencode(global_params), headers=headers)
+    return result.status_code == 200
+
+def cf_dns(token_id, token_key, domain, record, ip):
+    api_url = 'https://api.cloudflare.com/client/v4/'
+    params = {'name': domain,
+        'status': 'active'}
+    headers = {'X-Auth-Email': token_id,
+        'X-Auth-Key': token_key,
+        'Content-Type': 'application/json'}
+    domains = requests.get(api_url + 'zones', params=params, headers=headers).json()
+    domain_id = domains['result'][0]['id']
+
+    params = {'type': 'A', 'name': 'g.xdays.me'}
+    records = requests.get(api_url + 'zones/%s/dns_records' % domain_id,
+        params=params, headers=headers).json()
+    record_id = records['result'][0]['id']
+
+    data = {'type': 'A', 'name': record, 'content': ip}
+    result = requests.put(api_url + 'zones/%s/dns_records/%s' % (domain_id, record_id),
+        json=data, headers=headers)
+    return result.status_code == 200
  
 def get_public_ip():
-    sock = socket.create_connection(('ns1.dnspod.net', 6666))
-    ip = sock.recv(16)
-    sock.close()
+    ip = requests.get('http://ipip.tk/ip').text.strip('\n')
     return ip
 
 def get_local_ip():
@@ -55,19 +67,46 @@ def get_local_ip():
         0x8915,  # SIOCGIFADDR 
         struct.pack('256s', iface[:15]) 
     )[20:24])
+
+def get_env(var_name, default):
+    return os.environ.get(var_name, default)
  
 if __name__ == '__main__':
+    envs = ['PROVIDER', 'TOKEN_ID', 'TOKEN_KEY', 'DOMAIN', 'RECORD', 'IP_TYPE']
+    current_file = '/tmp/current'
+    for env in envs:
+        if env in os.environ:
+            exec('%s="%s"' % (env, get_env(env, None)))
+        else:
+            print '%s is required' % env
+            sys.exit(1)
+    if  os.path.exists(current_file):
+        with open(current_file, 'r') as f:
+            current_ip = f.read()
+    else:
+        current_ip = None
+ 
     try:
-        if type == 'public':
+        if IP_TYPE == 'public':
             ip = get_public_ip()
         else:
             ip = get_local_ip()
-        print ip
         if current_ip != ip:
-            if ddns(ip):
-                current_file = open('./current', 'w+')
-                current_file.write(ip)
-                current_file.close()
+            if PROVIDER == 'dnspod':
+                result = dp_dns(TOKEN_ID, TOKEN_KEY, DOMAIN, RECORD, ip)
+            elif PROVIDER == 'cloudflare':
+                result = cf_dns(TOKEN_ID, TOKEN_KEY, DOMAIN, RECORD, ip)
+            else:
+                result = None
+                print 'provider not support'
+            if result:
+                print 'update %s dns record successfully' % PROVIDER
+            else:
+                print 'failed to update %s dns record' % PROVIDER
+            with open(current_file, 'w+') as f:
+                f.write(ip)
+        else:
+            print 'no change with ip'
     except Exception, e:
         print e
         pass
